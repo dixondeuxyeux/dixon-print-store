4c2b950a90675af1517e3958e12751c08accfaa4
dixondeuxyeux <dixondeuxyeux@gmail.com>
2021-09-25 23:31:04 +0000

    go 3

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -6,7 +6,7 @@ const handler = nc()
 
 handler.get(async (req, res) => {
   await db.connect()
-  const products = await Product.findById({})
+  const products = await Product.find({})
   await db.disconnect()
   res.send(products)
 })
