d52b26ca9ea942ba5bd2aa48d809fcf1b6e165f6
dixondeuxyeux <dixondeuxyeux@gmail.com>
2021-09-25 19:36:04 +0000

    added is auth

--- pages/api/products/[id]/index.js
+++ pages/api/products/[id]/index.js
@@ -1,8 +1,10 @@
 import nc from 'next-connect'
+import { isAdmin, isAuth } from '../../../../utils/auth'
 import Product from '../../../../models/Product'
 import db from '../../../../utils/db'
 
 const handler = nc()
+handler.use(isAuth, isAdmin)
 
 handler.get(async (req, res) => {
   await db.connect()
