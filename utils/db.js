7fafbfdfcdfc1400c6fed49aadabdab0bea51b50
dixondeuxyeux <dixondeuxyeux@gmail.com>
2021-09-27 06:10:09 +0000

    back await

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connection = {}
 
-function connect() {
+async function connect() {
   if (connection.isConnected) {
     console.log('already connected')
     return
@@ -13,9 +13,9 @@ function connect() {
       console.log('use previous connection')
       return
     }
-     mongoose.disconnect()
+    await mongoose.disconnect()
   }
-  const db = mongoose.connect(process.env.MONGODB_URI, {
+  const db = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
